1e6f5d1ad8a11aec1368e3196947d489e9fd6753
nad971 <nadezhdakuprina8@gmail.com>
2026-04-22 10:01:19 +0300

    Update the database.py

--- the database.py
+++ the database.py
@@ -73,3 +73,89 @@ INSERT INTO progress (status, hours_played, rating) VALUES (?, ?, ?)
 cursor.execute("""
 INSERT INTO games (title, platform_id, genre, year, progress_id) VALUES (?, ?, ?, ?, ?)
 """)
+
+# Пользовательский ввод
+print(" Введите название игры: ")
+title = input()
+
+print("Введите платформу: ")
+platform_name = input()
+
+print(" Введите жанр: ")
+genre = input()
+
+print(" Введите год выпуска: ")
+year = int(input())
+
+print(" Введите статус : ")
+status = input()
+
+print("Введите сколько часов наиграно: ")
+hours = int(input())
+
+print(" Введите оценку (1-10): ")
+rating = int(input())
+
+cursor.execute("""
+INSERT INTO platforms (name, company) VALUES (?, ?)
+""",(title,platform_name,genre,year))
+
+cursor.execute("""
+INSERT INTO progress (status, hours_played, rating) VALUES (?, ?, ?)
+""",(status,hours,rating))
+
+#Заполнение таблиц в вручную
+cursor.execute("""
+INSERT INTO games (title,platform_name,genre,year)
+VALUES("Resident Evil 4","экшен","PC", 2026)
+""")
+cursor.execute("""
+INSERT INTO games (title,platform_name,genre,year)
+VALUES("The Sims 4","Симулятор"," PlayStation 4", 2014)
+""")
+cursor.execute("""
+INSERT INTO games (title,platform_name,genre,year)
+VALUES("The Elder Scrolls 5: Skyrim","фэнтези","PlayStation 3", 2011)
+""")
+cursor.execute("""
+INSERT INTO games (title,platform_name,genre,year)
+VALUES("Crimson Desert","ролевая игра","Xbox Series X", 2026)
+""")
+cursor.execute("""
+INSERT INTO games (title,platform_name,genre,year)
+VALUES("Perfect Dark","приключение","Xbox Game Studios", 2025)
+""")
+
+#Через случайные комбинации
+platform_array=[(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),()
+]
+cursor.execute("""
+INSERT INTO platforms (name, company) VALUES (?, ?)
+""",platform_array)
+
+progress_array=[(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),()
+]
+cursor.execute("""
+INSERT INTO progress (status, hours_played, rating) VALUES (?, ?, ?)
+""",progress_array)
+
+games_array=[(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),(),
+(),(),(),(),(),() 
+]
+cursor.execute("""
+INSERT INTO games (title,platform_id,genre,year,progress_id ) VALUES (?, ?, ?, ? ,?)
+""",games_array)
+
+connection.commit()
+connection.close()
